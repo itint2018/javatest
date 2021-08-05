286e6330f298c3e44a085d953652d21405141db8
Administrator <ivsnew@mail.ru>
2021-08-05 11:59:05 +0300

    First commit

---
+++ src/main/java/ru/itintego/javatest/models/User.java
@@ -0,0 +1,21 @@
+package ru.itintego.javatest.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "last_name", length = 100)
+    private String lastName;
+}
