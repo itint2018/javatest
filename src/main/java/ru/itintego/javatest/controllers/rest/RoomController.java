46068a82ce0af7859db6a757047ce4c6a09cffd9
TheLameres <xthe.lameresx@gmail.com>
2021-08-12 08:17:14 +0300

    Add rooms/{id}/reserve gets active user from SecurityContextHolder

--- src/main/java/ru/itintego/javatest/controllers/rest/RoomController.java
+++ src/main/java/ru/itintego/javatest/controllers/rest/RoomController.java
@@ -2,30 +2,36 @@ package ru.itintego.javatest.controllers.rest;
 
 import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import ru.itintego.javatest.dto.ReserveDto;
 import ru.itintego.javatest.models.ReserveRoom;
 import ru.itintego.javatest.models.Room;
+import ru.itintego.javatest.models.User;
 import ru.itintego.javatest.repositories.ReserveRoomRepository;
 import ru.itintego.javatest.repositories.RoomRepository;
+import ru.itintego.javatest.repositories.UserRepository;
 
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/rooms")
 public class RoomController implements DataController<Room, Long> {
     private final RoomRepository roomRepository;
     private final ReserveRoomRepository reserveRoomRepository;
+    private final UserRepository userRepository;
     private final Logger logger;
 
-    public RoomController(RoomRepository roomRepository, ReserveRoomRepository reserveRoomRepository, Logger logger) {
+    public RoomController(RoomRepository roomRepository, ReserveRoomRepository reserveRoomRepository, UserRepository userRepository, Logger logger) {
         this.roomRepository = roomRepository;
         this.reserveRoomRepository = reserveRoomRepository;
+        this.userRepository = userRepository;
         this.logger = logger;
     }
 
@@ -53,6 +59,7 @@ public class RoomController implements DataController<Room, Long> {
         LocalTime endTime = LocalTime.parse(reserveDto.getTimeEnd());
         LocalDateTime startDateTime = LocalDate.parse(reserveDto.getDate()).atTime(startTime);
         LocalDateTime endDateTime = LocalDate.parse(reserveDto.getDate()).atTime(endTime);
+        Optional<User> activeUser = userRepository.findByLogin(SecurityContextHolder.getContext().getAuthentication().getName());
         Long aLong = reserveRoomRepository.countAllByStartBetweenAndEndBetweenAndRoom(startDateTime, endDateTime, byId);
         if (aLong != 0) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Start date or end date is intersected");
@@ -62,6 +69,7 @@ public class RoomController implements DataController<Room, Long> {
         reserveRoom.setEnd(endDateTime);
         reserveRoom.setRoom(byId);
         reserveRoom.setDescription(reserveDto.getDescription());
+        reserveRoom.setUser(activeUser.orElseThrow(EntityNotFoundException::new));
         ReserveRoom save = reserveRoomRepository.save(reserveRoom);
         return save;
     }
